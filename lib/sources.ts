8364f1569c04b76a33bff70c36af04763c77c451
Aarshi Khemka <aarshikhemka@HYD-62310772.local>
2026-06-11 22:49:48 +0530

    Update: Use more reliable RSS news sources
    - NDTV Business and The Hindu for India business news
    - BBC News and Al Jazeera for global business news
    - Eventbrite and Events Industry Council for events
    - These sources have reliable, accessible RSS feeds

--- lib/sources.ts
+++ lib/sources.ts
@@ -1,15 +1,13 @@
 export const newsSources = [
-  // Business India - RSS Feeds
-  { name: 'Economic Times', url: 'https://feeds.economictimes.indiatimes.com/etopics/news', category: 'business_india' },
-  { name: 'Business Standard', url: 'https://www.business-standard.com/rss/rssfeed_latest.xml', category: 'business_india' },
-  { name: 'Hindu Business Line', url: 'https://www.thehindubusinessline.com/feed/latest/', category: 'business_india' },
+  // Business India
+  { name: 'NDTV Business', url: 'https://feeds.ndtv.com/ndtvbusiness.xml', category: 'business_india' },
+  { name: 'The Hindu', url: 'https://feeds.thehindu.co.in/business/latest', category: 'business_india' },
 
-  // Business Global - RSS Feeds
-  { name: 'Reuters Business', url: 'https://feeds.reuters.com/reuters/businessNews', category: 'business_global' },
-  { name: 'CNBC International', url: 'https://www.cnbc.com/id/100003114/device/rss/rss.html', category: 'business_global' },
-  { name: 'BBC Business', url: 'http://feeds.bbc.co.uk/news/business/rss.xml', category: 'business_global' },
+  // Business Global
+  { name: 'BBC News Business', url: 'http://feeds.bbc.co.uk/news/business/rss.xml', category: 'business_global' },
+  { name: 'Al Jazeera English', url: 'https://www.aljazeera.com/xml/rss/all.xml', category: 'business_global' },
 
-  // Events - RSS Feeds
-  { name: 'Event Magazine', url: 'https://www.eventmagazine.co.uk/feed/', category: 'events' },
-  { name: 'ISES Events', url: 'https://www.ises.org/news-and-events/news/rss', category: 'events' },
+  // Events
+  { name: 'Eventbrite Blog', url: 'https://www.eventbrite.com/blog/feed/', category: 'events' },
+  { name: 'Events Industry Council', url: 'https://www.eventscouncil.org/feed/', category: 'events' },
 ];
